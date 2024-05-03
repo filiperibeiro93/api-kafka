4304d2ec2eedad0c1c6a3398d532513159d4923a
Filipe Ribeiro <facribeiro.93@gmail.com>
2024-05-02 21:58:55 -0300

    criando client

---
+++ src/main/java/com/portal/apikafka/client/CarPostStoreClient.java
@@ -0,0 +1,43 @@
+package com.portal.apikafka.client;
+
+import com.portal.apikafka.dto.CarPostDto;
+import com.portal.apikafka.dto.OwnerPostDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+@Component
+public class CarPostStoreClient {
+
+    private final String USER_STORE_SERVICE_URI = "http://localhost:8080/user";
+    private final String POST_STORE_SERVICE_URI = "http://localhost:8080/sales";
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public CarPostStoreClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public List<CarPostDto> carForSaleClient() {
+        var responseEntity = restTemplate.getForEntity(POST_STORE_SERVICE_URI + "/cars", CarPostDto[].class);
+        return Arrays.asList(Objects.requireNonNull(responseEntity.getBody()));
+    }
+
+    public void ownerPostClient(OwnerPostDto newUser) {
+        restTemplate.postForEntity(USER_STORE_SERVICE_URI, newUser, OwnerPostDto.class);
+    }
+
+    public void changeCarForSaleClient(CarPostDto carPostDto, String id) {
+        restTemplate.put(POST_STORE_SERVICE_URI + "/car/" + id, CarPostDto.class);
+    }
+
+    public void deleteCarForSaleClient(String id) {
+        restTemplate.delete(POST_STORE_SERVICE_URI + "/car/" + id);
+    }
+
+}
--- src/main/java/com/portal/apikafka/service/CarPostStoreServiceImpl.java
+++ src/main/java/com/portal/apikafka/service/CarPostStoreServiceImpl.java
@@ -1,12 +1,21 @@
 package com.portal.apikafka.service;
 
+import com.portal.apikafka.client.CarPostStoreClient;
 import com.portal.apikafka.dto.CarPostDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CarPostStoreServiceImpl implements CarPostStoreService {
+
+    private final CarPostStoreClient carPostStoreClient;
+
+    @Autowired
+    public CarPostStoreServiceImpl(CarPostStoreClient carPostStoreClient) {
+        this.carPostStoreClient = carPostStoreClient;
+    }
 
     @Override
     public List<CarPostDto> getCarsForSale() {
