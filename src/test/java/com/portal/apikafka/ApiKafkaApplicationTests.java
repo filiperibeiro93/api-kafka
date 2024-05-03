717741bdc15df07ff6def72772ed9fd1d9b77879
Filipe Ribeiro <facribeiro.93@gmail.com>
2024-05-02 21:09:15 -0300

    first commit

---
+++ src/test/java/com/portal/apikafka/ApiKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.apikafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
